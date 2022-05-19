0b2b1515244e2b76c1dff1d3a7e47e25542c9f3e
Carmen Quan <cquan@launchdarkly.com>
2022-05-19 12:36:16 -0700

    update to v9

--- configuration.go
+++ configuration.go
@@ -102,7 +102,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/0/go",
+		UserAgent:        "OpenAPI-Generator/9/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
